328903aa76e09a2fab4991e992097dca5263401b
Ruben Morera <rubencho_01@hotmail.com>
2018-08-07 11:01:22 -0500

    Ajuste en el constructor de frmConsultaMovimientos

--- ModuloCredCob/frmConsultaMovimientos.vb
+++ ModuloCredCob/frmConsultaMovimientos.vb
@@ -21,7 +21,7 @@ Public Class frmConsultaMovimientos
     End Sub
 
     Public Sub New(URLGateway As String)
-        MyBase.New(4, Main.GLOBAL_IDUsuario, Main.GLOBAL_IDEmpleado, URLGateway)
+        MyBase.New(Main.GLOBAL_Modulo, Main.GLOBAL_IDUsuario, Main.GLOBAL_IDEmpleado, URLGateway)
         'This call is required by the Windows Form Designer.
         InitializeComponent()
         'Add any initialization after the InitializeComponent() call
